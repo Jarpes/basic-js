9b278b7c9a99f2e27a07d06c50d50bbe4a2690e6
Jaroslav <new.pustavit@gmail.com>
2021-09-10 14:44:59 +0600

    feat: complete (ST) File names

--- src/st-file-names.js
+++ src/st-file-names.js
@@ -15,7 +15,51 @@ import { NotImplementedError } from '../extensions/index.js';
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-export default function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function renameFiles(names) {
+  
+  function getAllIndexes(arr, val) {
+    var indexes = [], i;
+    for(i = 0; i < arr.length; i++)
+        if (arr[i] === val)
+            indexes.push(i);
+    return indexes;
+  }
+
+  function checkDuplicate(names) {
+    return names.filter(function(item, pos) {
+      return names.indexOf(item) == pos;
+    })
+  }
+  
+  function indexDuplicates(names) {
+    let uniquenames = checkDuplicate(names)
+    let duplicates = []
+    for (let word in uniquenames) {
+      duplicates.push(getAllIndexes(names, uniquenames[word]))
+    }
+    return duplicates
+  }
+
+  let trigger = true;
+  while (trigger) {
+    let indexes = indexDuplicates(names)
+    for (let checkIndex in indexes) {
+      indexes[checkIndex].shift()
+      if (indexes[checkIndex].length >= 1) {
+        let renameCounter = 1;
+        for (let updateIndex in indexes[checkIndex]) {
+          names[indexes[checkIndex][updateIndex]] = names[indexes[checkIndex][updateIndex]] + '(' + renameCounter + ')';
+          renameCounter++
+        }
+      }
+    }
+
+    let counter = 0;
+    for (let renameIndex in indexes) {
+      counter += indexes[renameIndex].length
+    }
+    if (counter == 1 || counter == 0) trigger = false;
+  }
+  
+  return names;с рекурс
 }
