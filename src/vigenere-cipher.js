f48460c7f828b79083939cc349dd11bf99e19b1a
Jaroslav <new.pustavit@gmail.com>
2021-09-18 03:46:23 +0600

    feat: complete Vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,41 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  checker(str) {
+    return str.length === 1 && str.toUpperCase().match(/[A-Z]/g);
+  }
+
+  encrypt(message, key) {
+    let vigenere = '';
+
+    if (!message || !key) throw new Error('Incorrect arguments!');
+  
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (this.checker(message.charAt(i))) {
+        vigenere += String.fromCharCode((key.toUpperCase().charCodeAt(j) + message.charAt(i).toUpperCase().charCodeAt(0) - 2 * 65) % 26 + 65);
+        j = ++j % key.length;
+      } else vigenere = vigenere + message.charAt(i);
+    }
+
+    return (this.direct == false) ? vigenere.split("").reverse().join("") : vigenere;
+  }
+
+  decrypt(encryptedMessage, key) {
+    let vigenere = '';
+
+    if (!encryptedMessage || !key) throw new Error('Incorrect arguments!');
+ 
+    for (let i = 0, j = 0; i < encryptedMessage.length; i++) {
+      if (this.checker(encryptedMessage.charAt(i))) {
+        vigenere += String.fromCharCode(90 - (25 - (encryptedMessage.charAt(i).charCodeAt(0) - key.toUpperCase().charCodeAt(j))) % 26);
+        j = ++j % key.length;
+      } else vigenere = vigenere + encryptedMessage.charAt(i);
+    }
+
+    return (this.direct == false) ? vigenere.split("").reverse().join("") : vigenere;
   }
 }
