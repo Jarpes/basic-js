b8cc723d644d76e36f8f638eeed4fe27e3e10b4c
Jaroslav <new.pustavit@gmail.com>
2021-09-18 01:43:53 +0600

    feat: complete Extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,30 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  if (!options.separator)           options.separator           = '+';
+  if (!options.additionSeparator)   options.additionSeparator   = '|';
+  if (!options.repeatTimes)         options.repeatTimes         = 1;
+  if (!options.additionRepeatTimes) options.additionRepeatTimes = 1;
+  if ((!options.addition && options.addition == false) || typeof options.addition == "object") options.addition = String(options.addition);
+  
+  let additionArray   = [];
+  let additionString  = str;
+  let fullArray       = [];
+  let fullString      = '';
+  str                 = '' + str || '';
+
+  for (let i = 1; i <= options.additionRepeatTimes; i++) {
+    additionArray.push(options.addition);
+  }
+  
+  additionString += additionArray.join(options.additionSeparator);
+
+  for (let i = 1; i <= options.repeatTimes; i++) {
+    fullArray.push(additionString);
+  }
+
+  fullString     = fullArray.join(options.separator);
+
+  return fullString;
 }
