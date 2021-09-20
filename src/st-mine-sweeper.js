5d5b3f58b076ee1a1c8a1c483f90ee0fa8c25199
Jaroslav <new.pustavit@gmail.com>
2021-09-20 21:28:24 +0600

    feat: complete (ST) Minesweeper

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,20 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper (matrix) {
+  let result = matrix.map((item) => 
+    item.map(() => 0
+  ));
+  let changeArray = ([item, second]) => [ [item - 1, second - 1], [item - 1, second], [item - 1, second + 1], [item, second - 1], [item, second + 1], [item + 1, second - 1], [item + 1, second], [item + 1, second + 1]];
+  for (let x = 0; x < matrix.length; x++) {
+    for (let y = 0; y < matrix[x].length; y++) {
+      if (matrix[x][y]) {
+        var changedArray = changeArray([x, y]);
+        for (let k = 0; k < 8; k++) {
+          if (changedArray[k][0] >= 0 && changedArray[k][1] >= 0 && changedArray[k][0] < matrix.length && changedArray[k][1] < matrix[x].length ) result[changedArray[k][0]][changedArray[k][1]]++
+        }
+      }
+    }
+  }
+  return result;
 }
